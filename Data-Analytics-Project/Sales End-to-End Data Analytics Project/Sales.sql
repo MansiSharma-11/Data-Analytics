e7f32c2a6e5ad420c91c650fdd1de6c439409836
MansiSharma11 <mansimsharma11@gmail.com>
2026-07-26 21:19:45 +0530

    new sql

---
+++ Data-Analytics-Project/Sales End-to-End Data Analytics Project/Sales.sql
@@ -0,0 +1,10 @@
+-- CREATE DATABASE sales;
+-- USE sales;
+-- SELECT * FROM cleaned_data;
+-- SHOW tables;
+-- ALTER TABLE cleaned_data RENAME TO sales_data;
+
+SELECT 
+	COUNT(*)
+FROM 
+	sales_data;
